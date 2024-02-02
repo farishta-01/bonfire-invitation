d3c0dcde277731c88720f3e44859b2573c5a0656
farishta-01 <122055202+farishta-01@users.noreply.github.com>
2024-02-02 19:08:44 +0500

    last update

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
+
   <React.StrictMode>
      <App />
-  </React.StrictMode>
- 
+    
+    
+  </React.StrictMode>,
 )
